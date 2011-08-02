38faae2ec507bf80e1956aea7c7e7393563d29a0
rajeev jha <jha.rajeev@gmail.com>
2011-08-02 19:38:48 +0530

    remove templates compiled folder from tracking

--- job/web/templates_c/toolbar/default.tmpl.en.php
+++
@@ -1,9 +0,0 @@
-
- <div style="border:1px solid red;padding:20px;">
-            <div>
-                 <a href="/user/login.php"> Login </a>
-                 &nbsp;|&nbsp;Do not have an acocunt?&nbsp;
-                 <a href="/user/register.php"> Registration </a>
-            </div>
-
-        </div>
