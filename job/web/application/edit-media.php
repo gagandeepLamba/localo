290b64c117a6cb39b35c1a6309a7cfa6e293eb5a
Rajeev Jha <jha.rajeev@gmail.com>
2011-10-14 15:56:54 +0530

    swfupload component styling

--- job/web/application/edit-media.php
+++ job/web/application/edit-media.php
@@ -121,15 +121,13 @@
                     debug: false,
 
                     // Button settings
-                    button_image_url: "/swfupload/images/TestImageNoText_65x29.png",
-                    button_width: "65",
-                    button_height: "29",
+                    
+					button_image_url: "/swfupload/images/XPButtonUploadText_61x22.png",
+                    button_width: "61",
+                    button_height: "22",
                     button_placeholder_id: "spanButtonPlaceHolder",
-                    button_text: '<span class="theFont">Upload</span>',
-                    button_text_style: ".theFont { font-size: 16; }",
-                    button_text_left_padding: 12,
-                    button_text_top_padding: 3,
-
+					button_cursor:SWFUpload.CURSOR.HAND,
+                    
                     // The event handler functions are defined in handlers.js
                     file_queued_handler : fileQueued,
                     file_queue_error_handler : fileQueueError,
@@ -184,7 +182,7 @@
                             </div>
                             
 
-                            <h3 class="mt20"> Documents  </h3>
+                            <h4 class="mt20"> Documents  </h4>
                             
                             <?php include($_SERVER['APP_WEB_DIR'] . '/inc/form/message.inc'); ?>
 
@@ -207,7 +205,10 @@
                                     <div id="divStatus">No Files Uploaded</div>
                                     <div class="pt10">
                                         <span id="spanButtonPlaceHolder"></span>
-                                        <input id="btnCancel" type="button" value="Cancel All Uploads" onclick="swfu.cancelQueue();" disabled="disabled" style="margin-left: 2px; font-size: 8pt; height: 29px;" />
+										<span>
+											<input class="uploadCancelButton" id="btnCancel" type="button" value="Cancel Upload" onclick="swfu.cancelQueue();" disabled="disabled"/>
+										</span>
+										
                                     </div>
 
                                     <!-- hidden fields -->
