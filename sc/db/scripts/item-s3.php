be746f86853e7076f765940a607d99b53cd68c1d
Rajeev Jha <jha.rajeev@gmail.com>
2012-03-07 17:28:24 +0530

    changes to item to s3 script

--- sc/db/scripts/item-s3.php
+++ sc/db/scripts/item-s3.php
@@ -8,36 +8,41 @@
     use \com\indigloo\Configuration as Config;
        
 	error_reporting(-1);
+    printf("Make backup of DB first \n");
+    exit ;
 
-    //get items
-    //$sql = "select id,images_json from sc_question order by id desc limit 1";
-    $sql = "select id,images_json from sc_question where id = 611";
-    $mysqli = MySQL\Connection::getInstance()->getHandle();
-    $rows = MySQL\Helper::fetchRows($mysqli, $sql);
+    $iter = 1 ;
+    while($iter <= 1 ){
+        $sql = " select id,images_json from sc_question order by id desc limit 20 ";
+        $mysqli = MySQL\Connection::getInstance()->getHandle();
+        $rows = MySQL\Helper::fetchRows($mysqli, $sql);
+
+
+        foreach($rows as $row) {
+            printf("processing row id %d \n" ,$row['id']);
+            $images = json_decode($row["images_json"]);
+            $data = array();
+
+            foreach($images as $image) {
+                printf("processing image id %d \n",$image->id);
+                //load media DB row
+                // update sc_question.id with new media DB Row
+                $sql = " select * from sc_media where id = ".$image->id ;
+                $mediaDBRow = MySQL\Helper::fetchRow($mysqli, $sql);
+                $mediaVO = \com\indigloo\media\Data::create($mediaDBRow);
+                array_push($data,$mediaVO);
+            }
+            
+            $strMediaVO = json_encode($data);
+            updateItem($mysqli,$row['id'],$strMediaVO);
+            sleep(1);
+        }
+
+        $iter++ ;
+    }
     
 
-    foreach($rows as $row) {
-        printf("processing row id %d \n" ,$row['id']);
-        $images = json_decode($row["images_json"]);
-        $data = array();
-
-        foreach($images as $image) {
-            printf("processing image id %d \n",$image->id);
-            //load media DB row
-            // update sc_question.id with new media DB Row
-            $sql = " select * from sc_media where id = ".$image->id ;
-            $mediaDBRow = MySQL\Helper::fetchRow($mysqli, $sql);
-            $mediaVO = \com\indigloo\media\Data::create($mediaDBRow);
-            array_push($data,$mediaVO);
-        }
-        
-        $strMediaVO = json_encode($data);
-        updateItem($mysqli,$row['id'],$strMediaVO);
-
-        sleep(1);
-    }
-
-    function updateItem($mysqli,$questionId,$strMediaVO) {
+      function updateItem($mysqli,$questionId,$strMediaVO) {
       
         $updateSQL = " update sc_question set images_json = ? where id = ? " ;
         $stmt = $mysqli->prepare($updateSQL);
